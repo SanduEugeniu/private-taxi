5eac221e69f40e713b05b19ef05a50a0342e115a
Eugenyu <eugeniu.sandu@gosocialdev.eu>
2022-08-23 23:10:50 +0300

    test geolocation

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,43 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-import {
-  BackgroundGeolocation,
-  BackgroundGeolocationConfig,
-  BackgroundGeolocationEvents, BackgroundGeolocationResponse
-} from '@awesome-cordova-plugins/background-geolocation/ngx';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage implements OnInit{
-  location;
-  count = 0;
-  constructor(private backgroundGeolocation: BackgroundGeolocation) {
-    const config: BackgroundGeolocationConfig = {
-      desiredAccuracy: 10,
-      stationaryRadius: 20,
-      distanceFilter: 30,
-      debug: true, //  enable this hear sounds for background-geolocation life-cycle.
-      stopOnTerminate: false, // enable this to clear background location settings when the app terminates
-    };
-
-    this.backgroundGeolocation.configure(config)
-      .then(() => {
-
-        this.backgroundGeolocation.on(BackgroundGeolocationEvents.location).subscribe((location: BackgroundGeolocationResponse) => {
-          console.log(location);
-          this.count = this.count + 1;
-          this.location = location;
-          this.backgroundGeolocation.finish();
-        });
-
-      });
-
-  }
-  ngOnInit() {
-
-    this.backgroundGeolocation.start();
-
+export class TabsPage {
+  constructor() {
   }
 }
